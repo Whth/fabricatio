91c3d77018ca522e88fbba4a677687aace991985
Whth <whthyotta@qq.com>
2025-02-19 20:06:52 +0800

    refactor(fabricatio): rewrite TemplateManager for efficiency and usability
    - Remove unnecessary methods and simplify existing ones
    - Use Handlebars directly for template management- Add active_loading parameter to enable/disable template auto-reloading
    - Improve template discovery and rendering processes
    - Update method signatures and functionality to better align with Handlebars

--- src/templates.rs
+++ src/templates.rs
@@ -1,16 +1,15 @@
 use handlebars::Handlebars;
+use pyo3::exceptions::PyRuntimeError;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 use std::collections::HashMap;
 use std::path::PathBuf;
 use walkdir::WalkDir;
 
-
 /// Python bindings for the TemplateManager struct.
 #[pyclass]
 pub struct TemplateManager {
     templates_dir: Vec<PathBuf>,
-    discovered_templates: HashMap<String, PathBuf>,
     handlebars: Handlebars<'static>,
     suffix: String,
 }
@@ -19,17 +18,17 @@ pub struct TemplateManager {
 impl TemplateManager {
     /// Create a new TemplateManager instance.
     #[new]
-    #[pyo3(signature = (template_dirs, suffix=None))]
-    fn new(template_dirs: Vec<Bound<'_, PyAny>>,suffix:Option<String>) -> PyResult<Self> {
+    #[pyo3(signature = (template_dirs, suffix=None, active_loading=None))]
+    fn new(template_dirs: Vec<Bound<'_, PyAny>>, suffix:Option<String>, active_loading:Option<bool>) -> PyResult<Self> {
         let template_dirs: Vec<PathBuf> = template_dirs
             .into_iter()
             .map(|dir| dir.call_method0("as_posix")?.extract::<String>().map(PathBuf::from))
             .collect::<PyResult<Vec<PathBuf>>>()?;
-
+        let mut hbs=Handlebars::new();
+        hbs.set_dev_mode(active_loading.unwrap_or(false));
         let mut manager = TemplateManager {
             templates_dir: template_dirs,
-            discovered_templates: HashMap::new(),
-            handlebars: Handlebars::new(),
+            handlebars: hbs,
             suffix:suffix.unwrap_or("hbs".to_string()) 
         };
         manager.discover_templates();
@@ -38,71 +37,62 @@ impl TemplateManager {
 
     #[getter]
     fn template_count(&self) -> usize {
-        self.discovered_templates.len()
-    }
-
-
-    #[getter]
-    fn templates(&self) -> Vec<String> {
-        self.discovered_templates.keys().cloned().collect()
+        self.handlebars.get_templates().len()
     }
 
 
     /// Discover the templates in the template directories.
     fn discover_templates(&mut self) {
-        let mut discovered = HashMap::new();
 
-        for dir in self.templates_dir.iter().rev() {
-            for entry in WalkDir::new(dir)
-                .into_iter()
-                .filter_map(|e| e.ok())
-                .filter(|e| e.file_type().is_file())
-                .filter(|e| e.path().extension().and_then(|s| s.to_str()) == Some(&self.suffix))
-            {
-                let path = entry.path();
-                if let Some(stem) = path.file_stem().and_then(|s| s.to_str()) {
-                    discovered.insert(stem.to_string(), path.to_path_buf());
-                }
-            }
-        }
+        self.handlebars.clear_templates();
 
-        self.discovered_templates = discovered;
+        self.discovered_templates_raw()
+            .iter()
+            .for_each(|path| {
+                self.handlebars.register_template_file(path.file_stem().unwrap().to_str().unwrap(),path).unwrap()
+            })
+
     }
 
-    fn get_template(&self, name: &str) -> PyResult<String> {
-        if let Some(path) = self.discovered_templates.get(name) {
-            let template = std::fs::read_to_string(path)?;
-            Ok(template)
-        } else {
-            Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
-                "Template not found".to_string(),
-            ))
-        }
-    }
-
-
-    fn get_template_source(&self, name: &str) -> PyResult<String> {
-        if let Some(path) = self.discovered_templates.get(name) {
-            Ok(path.to_string_lossy().to_string())
-        } else {
-            Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
-                "Template not found".to_string(),
-            ))
-        }
+    /// Get the source code of a template.
+    fn get_template_source(&self, name: &str) -> Option<String> {
+        let get:Vec<String>=self.discovered_templates_raw()
+            .iter().filter(|&e| {
+            e.file_stem().unwrap().to_string_lossy()==*name })
+            .map(|e| e.to_string_lossy().to_string()).
+            collect();
+        get.last().cloned()
     }
     /// Render a template with the given data.
     fn render_template(&self, name: &str, data: &Bound<'_, PyDict>) -> PyResult<String> {
         let data: HashMap<String, String> = data.extract()?;
-        if let Some(path) = self.discovered_templates.get(name) {
-            let template = std::fs::read_to_string(path)?;
-            self.handlebars
-                .render_template(&template, &data)
-                .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
-        } else {
-            Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
-                "Template not found".to_string(),
-            ))
-        }
+        self.handlebars.render(name,&data).map_err(|e| PyErr::new::<PyRuntimeError,_>(format!("{}", e)))
+
+    }
+
+    /// Render a template with the given data.
+    fn render_template_obj(&self, name: &str, data: &Bound<'_, PyDict>) -> PyResult<String> {
+        let data: HashMap<String, HashMap<String,String>> = data.extract()?;
+        self.handlebars.render(name,&data).map_err(|e| PyErr::new::<PyRuntimeError,_>(format!("{}", e)))
+
+    }
+}
+
+impl TemplateManager {
+
+    /// Returns a list of all discovered templates.
+    fn discovered_templates_raw(&self)->Vec<PathBuf>
+    {
+        self.templates_dir.iter().rev()
+            .flat_map(|dir|{
+                WalkDir::new(dir)
+                    .into_iter()
+                    .filter_map(|e| e.ok())
+                    .filter(|e| e.file_type().is_file())
+                    .filter(|e| e.path().extension().and_then(|s| s.to_str()) == Some(&self.suffix))
+                    .map(|e| e.path().to_path_buf())
+            })
+            .collect()
     }
 }
 
