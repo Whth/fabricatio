a21cf76601fc6cade17096e6d4b62fad3cc75e23
Whth <whthyotta@qq.com>
2025-02-20 09:19:44 +0800

    feat(llm_usages): implement code and documentation writing using LLM
    - Add WriteCode and WriteDocumentation actions- Update Role to handle coding and documentation

--- examples/llm_usages/llm_usage.py
+++ examples/llm_usages/llm_usage.py
@@ -1,40 +1,59 @@
 """Example of using the library."""
 
 import asyncio
-from typing import Any, Callable
 
-from fabricatio import Action, Role, Task, WorkFlow, logger
+from fabricatio import Action, Role, Task, WorkFlow
+from fabricatio.journal import logger
+from fabricatio.models.events import Event
 from fabricatio.parser import PythonCapture
 
-task = Task(name="say hello", goal="say hello", description="say hello to the world")
 
-
-class Talk(Action):
+class WriteCode(Action):
     """Action that says hello to the world."""
 
-    name: str = "talk"
+    name: str = "write code"
+    output_key: str = "written_code"
+
+    async def _execute(self, task_input: Task[str], **_) -> str:
+        return await self.aask_validate(
+            task_input.briefing,
+            validator=PythonCapture.capture,
+        )
+
+
+class WriteDocumentation(Action):
+    """Action that says hello to the world."""
+
+    name: str = "write documentation"
+    description: str = "write documentation for the code in markdown format"
     output_key: str = "task_output"
 
-    async def _execute(self, task_input: Task[str], **_) -> Any:
-        def _validator(res: str) -> Callable[[float, float], float] | None:
-            code = PythonCapture.capture(res)
-            exec(code, None, locals())
-            if "addup" in locals():
-                return locals().get("addup")
-            return None
-
-        func = await self.aask_validate(
-            "make a python function which can compute addition of two numbers, with good typing, the function name shall be `addup`"
-        )
-        logger.info("executing talk action")
-        return func
+    async def _execute(self, task_input: Task[str], **_) -> str:
+        return await self.aask(task_input.briefing, task_input.dependencies_prompt)
 
 
 async def main() -> None:
     """Main function."""
     role = Role(
-        name="talker", description="talker role", registry={task.pending_label: WorkFlow(name="talk", steps=(Talk,))}
+        name="Coder",
+        description="A python coder who can write code and documentation",
+        registry={
+            Event.instantiate_from("coding.*").push("pending"): WorkFlow(name="write code", steps=(WriteCode,)),
+            Event.instantiate_from("doc.*").push("pending"): WorkFlow(
+                name="write documentation", steps=(WriteDocumentation,)
+            ),
+        },
     )
+
+    prompt = "write a python cli app which can add a list of numbers writen in a file together,with detailed google style documentation."
+
+    proposed_task = await role.propose(prompt)
+    code = await proposed_task.move_to("coding").delegate()
+    logger.success(f"Code: \n{code}")
+
+    proposed_task = await role.propose(f"{code} \n\n write Readme.md file for the code.")
+    doc = await proposed_task.move_to("doc").delegate()
+    logger.success(f"Documentation: \n{doc}")
 
 
 if __name__ == "__main__":
