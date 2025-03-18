88a5179b131b0cdb573cca52cc2f8b6b43dac157
Whth <whthyotta@qq.com>
2025-03-18 22:56:56 +0800

    fix(actions): handle path resolution failure for data dumping
    - Add error handling for path resolution in DumpFinalizedOutput action
    - Use `ok` function to wrap path resolution and provide error message- Improve robustness and error reporting when dumping data to a file

--- python/fabricatio/actions/output.py
+++ python/fabricatio/actions/output.py
@@ -3,6 +3,7 @@
 from fabricatio.models.action import Action
 from fabricatio.models.generic import FinalizedDumpAble
 from fabricatio.models.task import Task
+from fabricatio.models.utils import ok
 
 
 class DumpFinalizedOutput(Action):
@@ -11,8 +12,11 @@ class DumpFinalizedOutput(Action):
     output_key: str = "dump_path"
 
     async def _execute(self, task_input: Task, to_dump: FinalizedDumpAble, **_) -> str:
-        dump_path = await self.awhich_pathstr(
-            f"{task_input.briefing}\n\nExtract a single path of the file, to which I will dump the data."
+        dump_path = ok(
+            await self.awhich_pathstr(
+                f"{task_input.briefing}\n\nExtract a single path of the file, to which I will dump the data."
+            ),
+            "Could not find the path of file to dump the data.",
         )
 
         to_dump.finalized_dump_to(dump_path)
