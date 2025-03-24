37ecc35bb68a4bdc40b82ff9a3b233ba16008245
Whth <whthyotta@qq.com>
2025-03-24 18:58:21 +0800

    feat(actions): add PersistentAll action to persist data to files
    - Implement PersistentAll class to persist all data to a specified directory
    - Add support for persisting objects that implement the PersistentAble interface
    - Include error handling and logging for better user feedback

--- python/fabricatio/actions/output.py
+++ python/fabricatio/actions/output.py
@@ -3,8 +3,9 @@
 from pathlib import Path
 from typing import Optional
 
+from fabricatio.journal import logger
 from fabricatio.models.action import Action
-from fabricatio.models.generic import FinalizedDumpAble
+from fabricatio.models.generic import FinalizedDumpAble, PersistentAble
 from fabricatio.models.task import Task
 from fabricatio.models.utils import ok
 
@@ -32,3 +33,37 @@ class DumpFinalizedOutput(Action):
         )
         ok(to_dump, "Could not dump the data since the path is not specified.").finalized_dump_to(dump_path)
         return dump_path.as_posix()
+
+
+class PersistentAll(Action):
+    """Persist all the data to a file."""
+
+    output_key: str = "persistent_count"
+
+    async def _execute(
+        self,
+        task_input: Optional[Task] = None,
+        persist_dir: Optional[str | Path] = None,
+        **cxt,
+    ) -> int:
+        persist_dir = Path(
+            persist_dir
+            or ok(
+                await self.awhich_pathstr(
+                    f"{ok(task_input, 'Neither `task_input` and `dump_path` is provided.').briefing}\n\nExtract a single path of the file, to which I will persist the data."
+                ),
+                "Can not find the path of file to persist the data.",
+            )
+        )
+
+        count = 0
+        if persist_dir.is_file():
+            logger.warning("Dump should be a directory, but it is a file. Skip dumping.")
+            return count
+
+        for k, v in cxt:
+            if isinstance(v, PersistentAble):
+                v.persist(persist_dir)
+                count += 1
+
+        return count
