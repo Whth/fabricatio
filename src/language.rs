dee8f10de18d441f630d833d6f023f75e0cb6083
Whth <whthyotta@qq.com>
2025-04-08 01:35:35 +0800

    feat(language): add functions to check language detection
    - Add is_chinese, is_english, is_japanese, is_korean, is_arabic, is_russian,
      is_german, is_french, is_hindi, is_italian, is_dutch, is_portuguese,  is_swedish, is_turkish, and is_vietnamese functions
    - Update detect_language function to use &str instead of String
    - Add corresponding function signatures to rust.pyi file

--- src/language.rs
+++ src/language.rs
@@ -28,13 +28,99 @@ pub(crate) fn convert_to_string_respectively(lang: Lang) -> String {
 /// detect the language of a string
 #[pyfunction]
 #[pyo3(signature = (string))]
-fn detect_language(string: String) -> String {
-    convert_to_string_respectively(dl(string.as_str()))
+fn detect_language(string: &str) -> String {
+    convert_to_string_respectively(dl(string))
 }
 
 
+#[pyfunction]
+fn is_chinese(string: &str) -> bool {
+    dl(string) == Lang::Cmn
+}
+
+#[pyfunction]
+fn is_english(string: &str) -> bool {
+    dl(string) == Lang::Eng
+}
+
+#[pyfunction]
+fn is_japanese(string: &str) -> bool {
+    dl(string) == Lang::Jpn
+}
+
+#[pyfunction]
+fn is_korean(string: &str) -> bool {
+    dl(string) == Lang::Kor
+}
+#[pyfunction]
+fn is_arabic(string: &str) -> bool {
+    dl(string) == Lang::Ara
+}
+#[pyfunction]
+fn is_russian(string: &str) -> bool {
+    dl(string) == Lang::Rus
+}
+#[pyfunction]
+fn is_german(string: &str) -> bool {
+    dl(string) == Lang::Deu
+}
+
+#[pyfunction]
+fn is_french(string: &str) -> bool {
+    dl(string) == Lang::Fra
+}
+
+#[pyfunction]
+fn is_hindi(string: &str) -> bool {
+    dl(string) == Lang::Hin
+}
+
+#[pyfunction]
+fn is_italian(string: &str) -> bool {
+    dl(string) == Lang::Ita
+}
+
+#[pyfunction]
+fn is_dutch(string: &str) -> bool {
+    dl(string) == Lang::Nld
+}
+
+#[pyfunction]
+fn is_portuguese(string: &str) -> bool {
+    dl(string) == Lang::Por
+}
+
+#[pyfunction]
+fn is_swedish(string: &str) -> bool {
+    dl(string) == Lang::Swe
+}
+
+#[pyfunction]
+fn is_turkish(string: &str) -> bool {
+    dl(string) == Lang::Tur
+}
+
+#[pyfunction]
+fn is_vietnamese(string: &str) -> bool {
+    dl(string) == Lang::Vie
+}
 /// register the module
 pub(crate) fn register(_: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(detect_language,m)?)?;
+    m.add_function(wrap_pyfunction!(detect_language, m)?)?;
+    m.add_function(wrap_pyfunction!(is_chinese, m)?)?;
+    m.add_function(wrap_pyfunction!(is_english, m)?)?;
+    m.add_function(wrap_pyfunction!(is_japanese, m)?)?;
+    m.add_function(wrap_pyfunction!(is_korean, m)?)?;
+    m.add_function(wrap_pyfunction!(is_arabic, m)?)?;
+    m.add_function(wrap_pyfunction!(is_russian, m)?)?;
+    m.add_function(wrap_pyfunction!(is_german, m)?)?;
+    m.add_function(wrap_pyfunction!(is_french, m)?)?;
+    m.add_function(wrap_pyfunction!(is_hindi, m)?)?;
+    m.add_function(wrap_pyfunction!(is_italian, m)?)?;
+    m.add_function(wrap_pyfunction!(is_dutch, m)?)?;
+    m.add_function(wrap_pyfunction!(is_portuguese, m)?)?;
+    m.add_function(wrap_pyfunction!(is_swedish, m)?)?;
+    m.add_function(wrap_pyfunction!(is_turkish, m)?)?;
+    m.add_function(wrap_pyfunction!(is_vietnamese, m)?)?;
     Ok(())
 }
