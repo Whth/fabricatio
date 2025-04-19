27f7a35f6060981b92605ad0cbbcef5be797b07a
Whth <whthyotta@qq.com>
2025-04-19 19:51:13 +0800

    fix(fabricatio.capabilities): correct loop range and update logging messages in async clued search
    - Update loop range to start from 1 and iterate up to max_round
    - Fix logging messages to reflect correct round numbers
    - Adjust error handling and capacity check logging

--- python/fabricatio/capabilities/advanced_rag.py
+++ python/fabricatio/capabilities/advanced_rag.py
@@ -24,7 +24,7 @@ class AdvancedRAG(RAG):
         **kwargs: Unpack[FetchKwargs],
     ) -> CitationManager:
         """Asynchronously performs a clued search based on a given requirement and citation manager."""
-        if max_round<=0:
+        if max_round <= 0:
             raise ValueError("max_round should be greater than 0")
         if max_round == 1:
             logger.warning(
@@ -33,21 +33,21 @@ class AdvancedRAG(RAG):
 
         refinery_kwargs = refinery_kwargs or {}
 
-        for i in range(max_round + 1, 1):
-            logger.info(f"Round [{i + 1}/{max_round}] search started.")
+        for i in range(1, max_round + 1):
+            logger.info(f"Round [{i}/{max_round}] search started.")
             ref_q = await self.arefined_query(
                 f"{cm.as_prompt()}\n\nAbove is the retrieved references in the {i - 1}th RAG, now we need to perform the {i}th RAG."
                 f"\n\n{requirement}",
                 **refinery_kwargs,
             )
             if ref_q is None:
-                logger.error(f"At round [{i + 1}/{max_round}] search, failed to refine the query, exit.")
+                logger.error(f"At round [{i}/{max_round}] search, failed to refine the query, exit.")
                 return cm
             refs = await self.aretrieve(ref_q, ArticleChunk, base_accepted, **kwargs)
 
             if (max_capacity := max_capacity - len(refs)) < 0:
                 cm.add_chunks(refs[0:max_capacity])
-                logger.debug(f"At round [{i + 1}/{max_round}] search, the capacity is not enough, exit.")
+                logger.debug(f"At round [{i}/{max_round}] search, the capacity is not enough, exit.")
                 return cm
 
             cm.add_chunks(refs)
