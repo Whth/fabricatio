903df20220cc7359a83fc53c4f2c6cde5c37c945
Whth <whthyotta@qq.com>
2025-05-03 15:23:01 +0800

    refactor(journal): remove Rich library integrations
    - Remove Rich pretty print and traceback installations
    - Keep loguru for logging as before

--- python/fabricatio/journal.py
+++ python/fabricatio/journal.py
@@ -4,10 +4,7 @@ import sys
 
 from fabricatio.rust import CONFIG
 from loguru import logger
-from rich import pretty, traceback
 
-pretty.install()
-traceback.install()
 logger.remove()
 logger.add(sys.stderr, level=CONFIG.debug.log_level)
 
