99c40274d02d39a9a3e54af1bbbe0bdf4aa06195
Whth <whthyotta@qq.com>
2025-05-06 12:23:29 +0800

    feat(models): add new modules and reorganize imports
    - Add new modules: generic, usages, kwargs_types
    - Reorganize import structure for better readability
    - Update __all__ list to include new modules and remove duplicates

--- python/fabricatio/__init__.py
+++ python/fabricatio/__init__.py
@@ -1,15 +1,17 @@
 """Fabricatio is a Python library for building llm app using event-based agent structure."""
 
+from fabricatio.rust import CONFIG, TEMPLATE_MANAGER, BibManager, Event
+
 from fabricatio import actions, capabilities, parser, toolboxes, workflows
 from fabricatio.journal import logger
-from fabricatio.models import extra
+from fabricatio.models import generic, usages, extra, kwargs_types
 from fabricatio.models.action import Action, WorkFlow
 from fabricatio.models.role import Role
 from fabricatio.models.task import Task
 from fabricatio.models.tool import ToolBox
-from fabricatio.rust import CONFIG, TEMPLATE_MANAGER, BibManager, Event
 
 __all__ = [
+    "kwargs_types",
     "CONFIG",
     "TEMPLATE_MANAGER",
     "Action",
@@ -26,4 +28,6 @@ __all__ = [
     "parser",
     "toolboxes",
     "workflows",
+    "usages",
+    "generic"
 ]
