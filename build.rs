36f86a6a45f563c41da27394818612cf7f950961
Whth <whthyotta@qq.com>
2025-05-10 03:07:57 +0800

    build: update proto package name and refactor build script
    - Update package name in tei.proto from 'tei' to 'tei.v1'
    - Modify build.rs to rename tei.v1.rs to tei.rs after compilation

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::path::Path;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .out_dir("src")
@@ -5,5 +8,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(false)
         .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
         .compile_protos(&["proto/tei.proto"], &["proto"])
+        .map(|_| {
+            if Path::new("src/tei.v1.rs").exists() {
+                fs::rename("src/tei.v1.rs", "src/tei.rs").expect("Failed to rename file")
+            }
+        })
         .map_err(|e| e.into())
 }
