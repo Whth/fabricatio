fe7cf4923205dcd4c81dff1e411c8a988e1ebf7d
Whth <whthyotta@qq.com>
2025-05-21 02:27:19 +0800

    refactor(fabricatio-actions): update import path for FromMapping
    - Change import path for FromMapping from fabricatio.models.generic to fabricatio_actions.models.generic

--- packages/fabricatio-actions/python/fabricatio_actions/actions/fs.py
+++ packages/fabricatio-actions/python/fabricatio_actions/actions/fs.py
@@ -3,8 +3,7 @@
 from pathlib import Path
 from typing import Any, List, Mapping, Self
 
-from fabricatio.models.generic import FromMapping
-
+from fabricatio_actions.models.generic import FromMapping
 from fabricatio_core.fs import safe_text_read
 from fabricatio_core.journal import logger
 from fabricatio_core.models.action import Action
