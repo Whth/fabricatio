436afe8b61fba86f8b03220b0a451df75237f3c3
Whth <whthyotta@qq.com>
2025-05-31 20:17:27 +0800

    ci(build): update GitHub release action and improve code documentation
    - Add `append_body` parameter to GitHub release action for better release note management
    - Enhance code documentation in `create_deck.py` by adding class docstring and import comments

--- packages/fabricatio-anki/python/fabricatio_anki/capabilities/create_deck.py
+++ packages/fabricatio-anki/python/fabricatio_anki/capabilities/create_deck.py
@@ -1,9 +1,10 @@
+"""Provide capabilities for creating a deck of cards."""
 
-from fabricatio_core.capabilities.propose import Propose   
+from fabricatio_core.capabilities.propose import Propose
+
 
 class CreateDeck(Propose):
-    
-    
+    """Create a deck of cards."""
+
     ...
-    
-    
+
