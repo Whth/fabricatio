b97b52a14d14b801897767cdaad319b9c28e9c36
Whth <whthyotta@qq.com>
2025-06-02 16:10:41 +0800

    feat(fabricatio-anki): add deck generation capabilities and update related components
    - Add GenerateDeck class with methods for creating decks, models, and templates
    - Update config.py to include new template for generating Anki card types- Increment version number to 0.1.0-dev19
    - Add new template file for generating Anki card types

--- packages/fabricatio-anki/python/fabricatio_anki/config.py
+++ packages/fabricatio-anki/python/fabricatio_anki/config.py
@@ -12,6 +12,8 @@ class AnkiConfig:
     make_deck_creation_proposal_template: str = "make_deck_creation_proposal"
     """Template name for make deck creation proposal."""
 
-
+    generate_anki_card_type_template: str = "generate_anki_card_type"
+    """Template name for generate anki card type."""
+    
 anki_config = CONFIG.load("anki", AnkiConfig)
 __all__ = ["anki_config"]
