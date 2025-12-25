d972fa337469ec781837e171b4dbfead7d56f653
Whth <whthyotta@qq.com>
2025-12-25 22:40:51 +0800

    refactor(webui): replace fabricatio-core logger with dedicated fabricatio-logger dependency
    - Updated import in lib.rs to use fabricatio_logger instead of fabricatio_core for init_logger_auto
    - Modified webui.rs to import all logger utilities from fabricatio_logger
    - Changed Cargo.toml dependency from fabricatio-core to fabricatio-logger
    - Updated Cargo.lock to reflect the dependency change
    - Removed fabricatio-core dependency in favor of the new logger crate
    - Maintained same logger initialization functionality with new dependency structure

--- packages/fabricatio-webui/src/lib.rs
+++ packages/fabricatio-webui/src/lib.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 
-use fabricatio_core::logger::init_logger_auto;
+use fabricatio_logger::init_logger_auto;
 mod webui;
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
