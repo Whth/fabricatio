9a0db4dd341f1b25a280ad44ecb80097c82e3f81
Whth <whthyotta@qq.com>
2026-01-04 03:59:58 +0800

    chore(version): bump package versions and update dependencies
    - Update error-mapping from 0.2.3-dev1 to 0.2.3
    - Update mcp-manager from 0.1.1-dev0 to 0.1.1
    - Update fabricatio-agent from 0.2.5-dev0 to 0.2.5
    - Update fabricatio-core from 0.5.1-dev2 to 0.5.1
    - Update fabricatio-memory from 0.3.0-dev0 to 0.3.1
    - Update fabricatio-tool from 0.8.4-dev0 to 0.8.4
    - Update root project from 0.24.0 to 0.24.1
    - Add serde_json and utils to Cargo.lock dependencies
    - Update fabricatio-core in uv.lock from 0.5.1.dev1 to 0.5.1.dev3
    - Update fabricatio-memory in uv.lock from 0.2.11.dev1 to 0.3.0.dev1
    - Reorder import statements in fabricatio-core lib.rs

--- packages/fabricatio-core/src/lib.rs
+++ packages/fabricatio-core/src/lib.rs
@@ -1,9 +1,9 @@
-#![cfg_attr(feature = "stubgen", allow(dead_code, unused, ))]
+#![cfg_attr(feature = "stubgen", allow(dead_code, unused,))]
 
 use cfg_if::cfg_if;
 use fabricatio_config::Config;
 use fabricatio_constants::*;
-use fabricatio_logger::{init_logger, Logger};
+use fabricatio_logger::{Logger, init_logger};
 
 mod event;
 mod hash;
