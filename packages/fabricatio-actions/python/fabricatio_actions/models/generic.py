9b7dc025be07d82221a52efd11df7f52da7ba60b
Whth <whthyotta@qq.com>
2026-05-19 00:21:36 +0800

    refactor(actions): add generic type parameters to FromMapping and FromSequence

--- packages/fabricatio-actions/python/fabricatio_actions/models/generic.py
+++ packages/fabricatio-actions/python/fabricatio_actions/models/generic.py
@@ -5,22 +5,22 @@ while `FromSequence` provides a method to generate a list of objects from a sequ
 """
 
 from abc import ABC, abstractmethod
-from typing import Any, List, Mapping, Sequence
+from typing import Any, List, Mapping, Sequence, Type
 
 
-class FromMapping(ABC):
+class FromMapping[V,T](ABC):
     """Class that provides a method to generate a list of objects from a mapping."""
 
     @classmethod
     @abstractmethod
-    def from_mapping[S](cls: S, mapping: Mapping[str, Any], **kwargs: Any) -> List[S]:
+    def from_mapping(cls, mapping: Mapping[str, V],/, **kwargs: Any) -> List[T]:
         """Generate a list of objects from a mapping."""
 
 
-class FromSequence(ABC):
+class FromSequence[V](ABC):
     """Class that provides a method to generate a list of objects from a sequence."""
 
     @classmethod
     @abstractmethod
-    def from_sequence[S](cls: S, sequence: Sequence[Any], **kwargs: Any) -> List[S]:
+    def from_sequence[S](cls: Type[S], sequence: Sequence[V],/, **kwargs: Any) -> List[S]:
         """Generate a list of objects from a sequence."""
