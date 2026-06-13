173dcdb08ddfb4c67ca8f66f3d1d98979f84d32c
Whth <whthyotta@qq.com>
2026-06-14 02:21:00 +0800

    refactor(webui): replace default Vue scaffolding with workflow editor views

--- packages/fabricatio-webui/frontend/src/views/HomeView.vue
+++ packages/fabricatio-webui/frontend/src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import NodePalette from '../components/NodePalette.vue'
+import NodeEditor from '../components/NodeEditor.vue'
+import NodeConfigPanel from '../components/NodeConfigPanel.vue'
+import ExecutionPanel from '../components/ExecutionPanel.vue'
+import { useWorkflowStore } from '@/stores/workflow'
+
+const wfStore = useWorkflowStore()
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="editor-layout">
+    <NodePalette />
+    <NodeEditor />
+    <NodeConfigPanel v-if="wfStore.selectedNodeId" />
+    <ExecutionPanel />
+  </div>
 </template>
+
+<style scoped>
+.editor-layout {
+  display: flex;
+  height: calc(100vh - 40px);
+  overflow: hidden;
+}
+</style>
