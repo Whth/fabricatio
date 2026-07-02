c75a9ba4015f2b85726869e7161740d9bb283f04
Whth <whthyotta@qq.com>
2026-07-03 01:05:38 +0800

    feat(rag): add QAModel and template settings for precise_chunk and enrich_qa

--- packages/fabricatio-rag/python/fabricatio_rag/config.py
+++ packages/fabricatio-rag/python/fabricatio_rag/config.py
@@ -13,6 +13,14 @@ class RagConfig:
     refined_query_template: str = "built-in/refined_query"
     """The name of the refined query template which will be used to refine a query."""
 
+    precise_chunk_template: str = "built-in/precise_chunk"
+    """"""
+
+    enrich_qa_template: str = "built-in/enrich_qa"
+    """Template for generating question-answer pairs from text chunks."""
+
+    mini_chunk_size: int = 128
+
 
 rag_config = CONFIG.load("rag", RagConfig)
 __all__ = ["rag_config"]
